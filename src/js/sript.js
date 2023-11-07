d8cb1a7d4b8169fa5c1c72aff8abe4984f498d92
Rostyk <kremen.rostyslav@gmail.com>
2023-11-07 15:57:36 +0100

    addphp

--- src/js/sript.js
+++ src/js/sript.js
@@ -19,14 +19,20 @@ $(document).ready(function(){
         $('.overlay, #consultation').fadeIn('slow');
     });
     $('.modal__close').on('click', function() {
-        $('.overlay, #consultation, #thanks, #order').fadeOut('slow')
+        $('.overlay, #consultation, #thanks, #order').fadeOut('slow');
     });
 
-    $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
-        $(this)
-          .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
-          .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+    $('.button_mini').on('click', function(){
+        $('.overlay, #order').fadeIn('slow')
     });
+
+    $('.button_mini').each(function(i) {
+        $(this).on('click', function() {
+            $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
+            $('.overlay, #order').fadeIn('slow');
+        })
+    });
+
 
 
     function toggleSlide(item) {
@@ -41,4 +47,58 @@ $(document).ready(function(){
 
     toggleSlide('.catalog-item__link')
     toggleSlide('.catalog-item__back')
+
+
+    function validateForms(form){
+        $(form).validate({
+            rules: {
+                name: {
+                    required: true,
+                    minlength: 2
+                },
+                phone: "required",
+                email: {
+                    required: true,
+                    email: true
+                }
+            },
+            messages: {
+                name: {
+                    required: "Пожалуйста, введите свое имя",
+                    minlength: jQuery.validator.format("Введите {0} символа!")
+                  },
+                phone: "Пожалуйста, введите свой номер телефона",
+                email: {
+                  required: "Пожалуйста, введите свою почту",
+                  email: "Неправильно введен адрес почты"
+                }
+            }
+        });
+    };
+
+    validateForms('#consultation-form');
+    validateForms('#consultation form');
+    validateForms('#order form');
+
+    $('input[name=phone]').mask("+7 (999) 999-99-99");
+
+    $('form').submit(function(){
+        e.preventDefault();
+
+        if (!$(this).valid()) {
+            return;
+        }
+
+        $.ajax({
+            type: "POST",
+            url: "mailer/smart.php",
+            data: $(this).serialize()
+        }).done(function(){
+            $(this).find("input").val("");
+            $('#consultation, #order').fadeOut();
+            $('.overlay, #thanks').fadeIn('slow');
+            $('form').trigger('reset');
+        });
+        return false;
+    });
 });
